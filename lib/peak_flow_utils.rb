c1152dd9351ebca22548a5fc9bb3e5e40fc18082
kaspernj <k@spernj.org>
2016-10-08 09:37:43 +0200

    Made the RSpec helper work

--- lib/peak_flow_utils.rb
+++ lib/peak_flow_utils.rb
@@ -1,2 +1,3 @@
 class PeakFlowUtils
+  autoload :RspecHelper, "#{File.dirname(__FILE__)}/peak_flow_utils/rspec_helper"
 end
--- lib/peak_flow_utils/rspec_helper.rb
+++ lib/peak_flow_utils/rspec_helper.rb
@@ -1,4 +1,45 @@
 class PeakFlowUtils::RspecHelper
-  def initialize
+  def initialize(args)
+    @groups = args.fetch(:groups)
+    @group_number = args.fetch(:group_number)
+  end
+
+  def group_files
+    return @group_files if @group_files
+
+    # Sort them so that they are sorted by file path in three groups so each group have an equal amount of controller specs, features specs and so on
+    group_orders = []
+    @groups.times do
+      group_orders << []
+    end
+
+    group_index = 0
+    files.sort.each do |file|
+      group_orders[group_index] << file
+      group_index += 1
+      group_index = 0 if group_index >= group_orders.length
+    end
+
+    @group_files = group_orders[@group_number]
+  end
+
+private
+
+  def dry_result
+    @dry_result ||= JSON.parse(`bundle exec rspec --dry-run --format json`)
+  end
+
+  def files
+    return @files if @files
+
+    @files = []
+    dry_result.fetch("examples").each do |example|
+      file_path = example.fetch("file_path")
+      file_path = file_path[2, file_path.length]
+
+      @files << file_path
+    end
+
+    @files
   end
 end
