cd369fbcfce7502f28d16906ddfa01246346ba6d
Kasper Johansen <kaspernj@gmail.com>
2020-02-02 09:28:30 +0100

    Fall back to sort by file name if points are the same (#49)

--- spec/services/peak_flow_utils/rspec_helper_spec.rb
+++ spec/services/peak_flow_utils/rspec_helper_spec.rb
@@ -22,4 +22,36 @@ describe PeakFlowUtils::RspecHelper do
     expect(ignore_models).to eq true
     expect(ignore_system).to eq false
   end
+
+  describe "#sorted_files" do
+    it "falls back to sort the files by name" do
+      helper = PeakFlowUtils::RspecHelper.new(groups: 4, group_number: 2)
+
+      expect(helper).to receive(:files).and_return(
+        "specs/file_b_spec.rb" => {
+          examples: 1,
+          path: "specs/file_b_spec.rb",
+          points: 1
+        },
+        "specs/file_c_spec.rb" => {
+          examples: 1,
+          path: "specs/file_c_spec.rb",
+          points: 1
+        },
+        "specs/file_a_spec.rb" => {
+          examples: 1,
+          path: "specs/file_a_spec.rb",
+          points: 1
+        }
+      )
+
+      files = helper.sorted_files.map { |file| file.fetch(:path) }
+
+      expect(files).to eq [
+        "specs/file_a_spec.rb",
+        "specs/file_b_spec.rb",
+        "specs/file_c_spec.rb"
+      ]
+    end
+  end
 end
