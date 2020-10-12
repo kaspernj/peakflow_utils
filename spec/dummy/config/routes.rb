e0e13018eaa35ae18e2db074877a61ca3f4147ab
kaspernj <kaspernj@gmail.com>
2020-10-12 13:44:36 +0200

    Fixed offence

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   mount PeakFlowUtils::Engine => "/peak_flow_utils"
 
   resources :notifier_errors, only: [] do
-    post :action_error, on: :collection
+    collection do
+      post :action_error
+    end
   end
 end
