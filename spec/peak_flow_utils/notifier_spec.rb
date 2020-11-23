8edb95e7d14c68a7b6d1aaa9b1c8e73855666de7
Kasper Stöckel <kaspernj@gmail.com>
2020-11-23 10:54:13 +0100

    Fixed current was nil (#109)
    * Fixed current was nil
    
    * Fixed blank line after before offence

--- spec/peak_flow_utils/notifier_spec.rb
+++ spec/peak_flow_utils/notifier_spec.rb
@@ -3,7 +3,35 @@ require "rails_helper"
 describe PeakFlowUtils::Notifier do
   let(:notifier) { PeakFlowUtils::Notifier.new(auth_token: "stub") }
 
+  before { reset_notifier_configuration }
+
+  after { reset_notifier_configuration }
+
+  def reset_notifier_configuration
+    PeakFlowUtils::Notifier.instance_variable_set(:@current, nil)
+  end
+
+  describe "#configure" do
+    it "sets the current notifier" do
+      PeakFlowUtils::Notifier.configure(auth_token: "test-token")
+
+      expect(PeakFlowUtils::Notifier.current.auth_token).to eq "test-token"
+    end
+  end
+
+  describe "#current" do
+    it "raises an error if it isn't been configured" do
+      expect { PeakFlowUtils::Notifier.current }
+        .to raise_error(PeakFlowUtils::Notifier::NotConfiguredError, "Hasn't been configured")
+    end
+  end
+
   describe "#notify" do
+    it "raises an error if not configured" do
+      expect { PeakFlowUtils::Notifier.notify(error: "something") }
+        .to raise_error(PeakFlowUtils::Notifier::NotConfiguredError, "Hasn't been configured")
+    end
+
     it "sends the error to the server" do
       expect_any_instance_of(Net::HTTP).to receive(:request).and_return(
         double( # rubocop:disable RSpec/VerifiedDoubles
