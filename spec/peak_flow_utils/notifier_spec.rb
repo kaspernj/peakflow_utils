c00efb8de53485afb759d71a9effa1e48a92dc09
kaspernj <kaspernj@gmail.com>
2020-11-23 12:14:57 +0100

    Raise error if Peakflow rejects

--- spec/peak_flow_utils/notifier_spec.rb
+++ spec/peak_flow_utils/notifier_spec.rb
@@ -2,6 +2,17 @@ require "rails_helper"
 
 describe PeakFlowUtils::Notifier do
   let(:notifier) { PeakFlowUtils::Notifier.new(auth_token: "stub") }
+  let(:sample_error) do
+    error = nil
+
+    begin
+      raise "stub"
+    rescue => e # rubocop:disable Style/RescueStandardError
+      error = e
+    end
+
+    error
+  end
 
   before { reset_notifier_configuration }
 
@@ -34,24 +45,37 @@ describe PeakFlowUtils::Notifier do
 
     it "sends the error to the server" do
       expect_any_instance_of(Net::HTTP).to receive(:request).and_return(
-        double( # rubocop:disable RSpec/VerifiedDoubles
+        instance_double(
+          Net::HTTPResponse,
           body: JSON.generate(
             url: "https://www.peakflow.io/something"
-          )
+          ),
+          code: 200
         )
       )
 
-      error = nil
-
-      begin
-        raise "stub"
-      rescue => e # rubocop:disable Style/RescueStandardError
-        error = e
-      end
-
-      response = notifier.notify(error: error)
+      response = notifier.notify(error: sample_error)
 
       expect(response.url).to eq "https://www.peakflow.io/something"
+    end
+
+    it "raises an error if peakflow rejects" do
+      fake_response = instance_double(
+        Net::HTTPResponse,
+        body: JSON.generate(
+          errors: ["Test error", "Another error"]
+        ),
+        code: 401
+      )
+      expect(fake_response).to receive(:[]).with("content-type").and_return("application/json")
+
+      expect_any_instance_of(Net::HTTP).to receive(:request).and_return(fake_response)
+
+      expect { notifier.notify(error: sample_error) }
+        .to raise_error(
+          PeakFlowUtils::Notifier::FailedToReportError,
+          "Couldn't report error to Peakflow (code 401): Test error. Another error"
+        )
     end
   end
 end
