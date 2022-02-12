3b4634766d20a7492740525e1ae3e9812b2237df
kaspernj <k@spernj.org>
2022-02-12 16:10:20 +0000

    Fixed offences

--- lib/peak_flow_utils/thread_callbacks_patch.rb
+++ lib/peak_flow_utils/thread_callbacks_patch.rb
@@ -2,19 +2,18 @@ class Thread
   alias_method :_initialize, :initialize # rubocop:disable Style/Alias
 
   def self.on_initialize(&callback)
-    @@on_initialize_count = 0 if @on_initialize_count.nil?
+    @@on_initialize_count = 0 if @on_initialize_count.nil? # rubocop:disable Style/ClassVars
     count_to_use = @@on_initialize_count
-    @@on_initialize_count += 1
+    @@on_initialize_count += 1 # rubocop:disable Style/ClassVars
 
-    @@on_initialize_callbacks ||= {}
+    @@on_initialize_callbacks ||= {} # rubocop:disable Style/ClassVars
     @@on_initialize_callbacks[count_to_use] = callback
 
     count_to_use
   end
 
   def initialize(*args, &block)
-    @@on_initialize_callbacks ||= {}
-
+    @@on_initialize_callbacks ||= {} # rubocop:disable Style/ClassVars
     @@on_initialize_callbacks.each_value do |callback|
       callback.call(parent: Thread.current, thread: self)
     end
